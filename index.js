a1afc1d537f973c26f15216c478c41ba561d3f4d
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-11-24 11:21:11 -0300

    Obtener los contactos con los que se ha conversado

--- index.js
+++ index.js
@@ -20,14 +20,30 @@ const main = () => {
         process.exit(1)
     }
 
-    let usuario
+    let datos_usuario
     let ruta_intermedia = fs.readdirSync(ruta_usuario)
     if (ruta_intermedia.length) {
         const configuracion_usuario = path.join(ruta_usuario, ruta_intermedia[0], 'user_info.json')
         if (!fs.existsSync(configuracion_usuario)) {
             console.log(`No existe la información del usuario en ${configuracion_usuario}`)
         }
-        usuario = JSON.parse(fs.readFileSync(configuracion_usuario))
+        datos_usuario = JSON.parse(fs.readFileSync(configuracion_usuario))
+    }
+
+    for (let chat of fs.readdirSync(ruta_chats)) {
+        const ruta_chat = path.join(ruta_chats, chat)
+        const ruta_mensajes_chat = path.join(ruta_chat, 'messages.json')
+        if (fs.existsSync(ruta_mensajes_chat)) {
+            const ruta_informacion_chat = path.join(ruta_chat, 'group_info.json')
+            const data = JSON.parse(fs.readFileSync(ruta_informacion_chat))
+            const nombre_usuario = datos_usuario['user']['name']
+            const correo_usuario = datos_usuario['user']['email']
+            for (let usuario of data['members']) {
+                if (usuario['name'] !== nombre_usuario && usuario['email'] !== correo_usuario) {
+                    console.log(usuario['name'])
+                }
+            }
+        }
     }
 }
 
